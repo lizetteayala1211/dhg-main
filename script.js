5f63a20e8989a0eed5bb2cf84fb917c4d418a7d3
Lizette Ayala <lizetteayala1211@gmail.com>
2024-08-07 14:01:05 -0400

    updates to index & objects pages - formatting

--- script.js
+++ script.js
@@ -73,6 +73,36 @@ $(document).ready(function () {
   }
   );
 
+  ////// Zoom in on Object Images when Clicked ///////
+  $('img[data-enlargeable]').addClass('img-enlargeable').click(function() {
+    var src = $(this).attr('src');
+    var modal;
+  
+    function removeModal() {
+      modal.remove();
+      $('body').off('keyup.modal-close');
+    }
+    modal = $('<div>').css({
+      background: 'RGBA(0,0,0,.5) url(' + src + ') no-repeat center',
+      backgroundSize: 'contain',
+      width: '100%',
+      height: '100%',
+      position: 'fixed',
+      zIndex: '10000',
+      top: '0',
+      left: '0',
+      cursor: 'zoom-out'
+    }).click(function() {
+      removeModal();
+    }).appendTo('body');
+    //handling ESC
+    $('body').on('keyup.modal-close', function(e) {
+      if (e.key === 'Escape') {
+        removeModal();
+      }
+    });
+  });
+
   ////// Object Image Carousel(s) ///////
 
   // DOM utility functions:
