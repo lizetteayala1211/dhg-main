5135f8d00fb8f353f7a306ad997c0f17b1961e45
Lizette Ayala <lizetteayala1211@gmail.com>
2024-09-13 00:07:26 -0400

    archives zoom updated to be the same as objects zoom

--- script.js
+++ script.js
@@ -60,41 +60,6 @@ $(document).ready(function () {
     });
   });
 
-  ////// Archive Image(s) Fullscreen ///////
-
-  // Makes the clicked image full screen (uses a <div> with a background image)
-  $('.img-full-screen').click(function () {
-
-    // Prevents scrolling
-    $('body').addClass('scroll-disabled');
-
-    // Optional: Enables pinch and zoom
-    $('meta[name=viewport]').attr('content', 'width = device-width, initial-scale = 1.00, minimum-scale = 1.00, maximum-scale = 2.00, user-scalable=yes');
-
-    // Get image path source
-    let imagePath = $(this).attr('src');
-
-    // Set image path source
-    $('.img-placeholder').attr('style', 'background-image: url(' + imagePath + ')');
-
-    // Show image
-    $('.img-placeholder').fadeIn();
-
-  });
-
-  $('.img-placeholder').click(function () {
-
-    // Enables scrolling again
-    $('body').removeClass('scroll-disabled');
-
-    // Optional: Disables pinch and zoom
-    $('meta[name=viewport]').attr('content', 'width = device-width, initial-scale = 1.00, minimum-scale = 1.00, maximum-scale = 1.00');
-
-    // Hide image
-    $('.img-placeholder').fadeOut();
-  });
-
-
   ////// Object Image Carousel(s) ///////
 
   // DOM utility functions:
