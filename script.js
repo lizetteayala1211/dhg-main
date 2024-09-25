dd2ce600b7dcfe9e5f88b50af2fc135dd3b24772
Lizette Ayala <lizetteayala1211@gmail.com>
2024-09-25 09:27:47 -0400

    en/sp toggle amended to track local storage

--- script.js
+++ script.js
@@ -194,24 +194,43 @@ $(document).ready(function () {
   els(".carousel").forEach(carousel);
 
 
+
+
 ////// Language Toggle EN/SP Global //////
 
-  // Switching to English
-  $(".language-en-button").on("click", function () {
+function setLanguageToEnglish() {
 
-    $('[lang="sp"]').hide();
-    $('#landing [lang="en"]').css('display', 'block');
-    $('#global [lang="en"]').css('display', 'block');
-  });
+  $('[lang="sp"]').hide();
+  $('#landing [lang="en"]').css('display', 'block');
+  $('#global [lang="en"]').css('display', 'block');
+  $('p.en.toggleable-english-text').css('display', 'block');
+  $('button.en.toggleable-english-text').css('display', 'block');
+
+  if (window.localStorage) { localStorage.setItem("language", "english"); }
+}
+function setLanguageToSpanish() {
+  $('[lang="sp"]').show();
+  $('#landing [lang="en"]').css('display', 'none');
+  $('#global [lang="en"]').css('display', 'none');
+  $('p.en.toggleable-english-text').css('display', 'none');
+  $('button.en.toggleable-english-text').css('display', 'none');
+  $('button.sp.toggleable-spanish-text').css('display', 'block');
+
+
+  if (window.localStorage) { localStorage.setItem("language", "spanish"); }
+  // localStorage.language = "spanish"
+}
+
+if (window.localStorage && localStorage.getItem("language") == "spanish") {
+  setLanguageToSpanish()
+}
+
+  // Switching to English
+  $(".language-en-button").on("click", setLanguageToEnglish);
 
   // Switching to Spanish
-  $(".language-sp-button").on("click", function () {
+  $(".language-sp-button").on("click", setLanguageToSpanish);
 
-      $('[lang="sp"]').show();
-      $('#landing [lang="en"]').css('display', 'none');
-      $('#global [lang="en"]').css('display', 'none');
-
-  });
 
 
 ////// Show/Hide (Object Inner Pages) Button Toggle ///////
